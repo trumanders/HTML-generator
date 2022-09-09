3e7a1647c5c2362653b86170df4b6969ac2570f9
trumanders <trumanders@gmail.com>
2022-09-09 09:42:23 +0200

    Added correcting text for-loop

--- HTML-generator/Program.cs
+++ HTML-generator/Program.cs
@@ -1,14 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string[] apiInput = {"C#", "Databaser", "Webb", "Clean code"};
+string[] apiInput = {"C#", "DataBaser", "WEbb", "CleAn cODE"};
 
 string str1 = "<!DOCTYPE html>\n<html>\n<body>\n<main>";
 string str2 = "<h1> Välkomna! </h1>";
 string str3 = "";
 foreach (string str in apiInput)
 {
-    str3 += $"<h1> Kurs om {str}</p>\n";
+    string tmp = "";
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (i == 0) tmp += str[i].ToString().ToUpper();
+        else tmp += str[i].ToString().ToLower();
+    }
+    tmp = tmp.Trim();
+    str3 += $"<h1> Kurs om {tmp}</p>\n";
 }
 string str4 = "</main>\n</body>\n</html>";
 
