40b008f90611e34726744f24d64b1bacc1eb7260
trumanders <trumanders@gmail.com>
2022-09-09 09:11:28 +0200

    Add project files.

---
+++ HTML-generator/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string str1 = "<!DOCTYPE html>\n<html>\n<body>\n<main>";
+string str2 = "<h1> Välkomna! </h1>";
+string str3 = "<p> Kurs om C#</p>\n<p> Kurs om databaser</p>";
+string str4 = "</main>\n</body>\n</html>";
+
+Console.WriteLine($"{str1}{str2}{str3}{str4}");
