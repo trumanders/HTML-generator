0196ea3f39563d3a62b250c2cb67f888701bf79f
trumanders <trumanders@gmail.com>
2022-09-09 09:19:49 +0200

    Added foreach-loop for courses

--- HTML-generator/Program.cs
+++ HTML-generator/Program.cs
@@ -1,8 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+string[] apiInput = {"C#", "Databaser", "Webb", "Clean code"};
+
 string str1 = "<!DOCTYPE html>\n<html>\n<body>\n<main>";
 string str2 = "<h1> Välkomna! </h1>";
-string str3 = "<p> Kurs om C#</p>\n<p> Kurs om databaser</p>";
+string str3 = "";
+foreach (string str in apiInput)
+{
+    str3 += $"<h1> Kurs om {str}</p>\n";
+}
 string str4 = "</main>\n</body>\n</html>";
 
-Console.WriteLine($"{str1}\n{str2}\n{str3}\n{str4}\n");
+Console.WriteLine($"{str1}\n{str2}\n{str3}{str4}\n");
